cc94e07c23740b3297f4d19ba057b3dfe5c6f8fe
Eduardo <eduardo.klock@gmail.com>
2024-11-04 18:20:09 -0300

    Add function to redraw the squares and add flex property in styles.
    The function resetSquareDivs erase the old sketch from the html page and redraw them using the function createSquareDivs.
    The sketch use flex now to resize the squares proportionally to the number set by the user through the button.
    The elements on the body are now centralized.

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-function createSquareDivs() {
+function createSquareDivs(squaresNumber) {
     const container = document.querySelector("#container")
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squaresNumber; i++) {
         const row = document.createElement("div");
         row.setAttribute("id", "row");
         container.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < squaresNumber; j++) {
             const squareDiv = document.createElement("div");
             squareDiv.setAttribute("id", "square")
             row.appendChild(squareDiv);
         }
     }
+
+    (function paintSquare() {
+        const squares = document.querySelectorAll("#square");
+        squares.forEach((square) => {
+            square.addEventListener("mouseover", (event) => {
+                event.target.style.backgroundColor = "black";
+            })
+        })
+    })();
+};
+
+function resetSquareDivs(squaresNumber) {
+    const container = document.querySelector("#container");
+    container.textContent = "";
+    createSquareDivs(squaresNumber);
 }
 
-createSquareDivs();
+
+createSquareDivs(16);
+
+(function newSketch() {
+    const button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        let squaresNumber;
+        do {
+            squaresNumber = Number(prompt("Squares per side:"));
+            console.log(squaresNumber);
+        } while(squaresNumber < 1 || squaresNumber > 100 || !Number.isInteger(squaresNumber));
+        resetSquareDivs(squaresNumber);
+         
+    })
+})();
+
+
+
